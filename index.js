63d41332b0ee7235a93d2dd0e6aac53446bdb233
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-06-21 14:04:23 -0400

    Adde .gitignore file and dotenv npm package

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const inquirer = require("inquirer");
-const mysql2 = require("mysql2");
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  console.log(`Connected to the employee_db database.`)
+);
